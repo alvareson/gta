b7d59770b34b1515e98ac4e4f390361862d4451d
Alister <a.enikeev@mirador.software>
2024-09-30 19:28:10 +0400

    feat(app): update about us page

--- pages/about-us.vue
+++ pages/about-us.vue
@@ -1,17 +1,16 @@
 <template>
   <div class="about-us-page">
     <header class="about-us-page__header">
-      <h1 class="about-us-page__title">GTA Realty : Your Trustwothy Partner</h1>
       <p class="about-us-page__subtitle">
-        At GTA Realty, we're synonymous with reliability and trustworthiness in real estate. Our commitment is steadfast: to keep you informed, whether it's about promising opportunities or potential pitfalls. Just as the industry's best, we pride ourselves on being a beacon of reliability. With a keen eye for discerning the best deals and a dedication to transparency, we ensure that every step of your real estate journey is guided by integrity.
+        At GTA Realty, we believe that every client deserves an extraordinary real estate experience. With our international reach and deep understanding of the market, we offer a unique blend of local expertise and global perspective. Founded by a visionary Emirati entrepreneur, our company is dedicated to delivering unparalleled service and exceptional results. Here are three compelling reasons to choose GTA Realty for your real estate needs:
       </p>
     </header>
     <div class="about-us-page__body">
       <section class="about-us-page__section">
         <div class="about-us-page__text">
-          <h2>Elite connections, Expert insight</h2>
+          <h2>Global Expertise, Local Insight</h2>
           <p>
-            GTA Realty has over two decades of experience and brings unique expertise to the table, particularly in our seamless collaboration with international partners. We offer a curated selection of luxury properties coupled with unparalleled service. Our team of specifically selected experts ensures expert guidance every step of the way. At GTA Realty, we excel in the prime and super-prime real estate market, leveraging our global network to consistently deliver exceptional service. Each member of our team contributes their wealth of expertise and skills, fostering collaboration and synergy to ensure our clients' success. Additionally, we maintain exclusive and privileged contacts with developers, enhancing our ability to secure the finest properties for our clients.            
+            With a strong international presence, we bring a wealth of knowledge from diverse markets while maintaining a deep understanding of local trends. This combination allows us to craft tailored strategies that match global opportunities with local needs, ensuring you make informed decisions whether buying or selling.
           </p>
         </div>
         <div class="about-us-page__image">
@@ -23,9 +22,9 @@
           <img src="/img/aboutus2.jpg" alt="On-Demand and On-Location" />
         </div>
         <div class="about-us-page__text">
-          <h2>On-Demand and On-Location</h2>
+          <h2>⁠Integrity and Trust</h2>
           <p>
-            As a key player in the luxury real estate industry worldwide, MG Partners is dedicated to providing tailor-made services across the globe, utilizing our exclusive network to offer a personalized touch to every endeavor. Our commitment to each client ensures top-tier guidance and recommendations to facilitate optimal property choices. Leveraging cutting-edge marketing strategies and advanced technology, we ensure your luxury property stands out prominently, attracting the most relevant buyers.
+            At GTA Realty, integrity is at the core of everything we do. Our founder instilled the values of reliability and transparency, ensuring that our clients always feel confident and informed. We prioritize your best interests, fostering a relationship built on trust and open communication throughout your real estate journey.
           </p>
         </div>
       </section>
@@ -34,23 +33,96 @@
           <img src="/img/aboutus4.jpg" alt="Enhancing Value" />
         </div>
         <div class="about-us-page__text">
-          <h2>Enhancing Value Through Exemplary Standards</h2>
+          <h2>Exceptional Client-Centric Service</h2>
           <p>
-            Wherever you may be, the MG Partners team possesses the skill to actualize your aspirations within the sphere of luxury real estate. Beyond their extensive proficiency in local and global residential sales, our experts can provide support across diverse domains such as rentals, property management, new development sales, investment, and portfolio management. At MG Partners, we advocate for a distinctive perspective on luxury real estate—one that places the client at the core of all our endeavors.
+            Our mission is to provide a personalized approach to every client. We understand that real estate transactions can be complex, and we are here to simplify the process for you. Our dedicated team is committed to offering continuous support, expert advice, and an attentive ear, ensuring that your experience is smooth, satisfying, and memorable.
+            Choose GTA Realty and experience a real estate partnership that truly puts you first!
           </p>
         </div>
       </section>
     </div>
-    <div class="about-us-page__footer">
-      <p>OUR LEGACY. OUR TRADITION. YOUR BENEFIT.</p>
+    <div class="about-us-page__team">
+      <h2>Our Team</h2>
+      <Swiper
+        class="about-us-page__swiper"
+        :modules="[]"
+        :slides-per-view="5"
+        :breakpoints="{
+          320: {
+            slidesPerView: 1,
+            spaceBetween: 10
+          },
+          480: {
+            slidesPerView: 2,
+            spaceBetween: 10
+          },
+          768: {
+            slidesPerView: 3,
+            spaceBetween: 10
+          },
+          1024: {
+            slidesPerView: 4,
+            spaceBetween: 10
+          },
+          1280: {
+            slidesPerView: 5,
+            spaceBetween: 10
+          }
+        }"
+      >
+        <SwiperSlide v-for="(employee, index) in listOfEmployees" :key="index">
+          <div class="employee-card">
+            <img :src="employee.image" :alt="employee.name" />
+            <h3>{{ employee.name }}</h3>
+            <p>{{ employee.position }}</p>
+          </div>
+        </SwiperSlide>
+      </Swiper>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { onMounted, onUnmounted } from 'vue'
+import { ref, onMounted, onUnmounted } from 'vue'
+import { Swiper, SwiperSlide } from 'swiper/vue'
+import { Navigation } from 'swiper/modules'
+import 'swiper/css'
+import 'swiper/css/navigation'
 
 let observer: IntersectionObserver | null = null
+
+const listOfEmployees = [
+  {
+    name: 'Sulaiman Almulla',
+    position: 'Chairman',
+    image: '/img/stuff/sulaiman.png'
+  },
+  {
+    name: 'Ali Koko',
+    position: 'Chief Executive Officer',
+    image: '/img/stuff/ali.png'
+  },
+  {
+    name: 'Hussein Abdelhameed',
+    position: 'Accountant',
+    image: '/img/stuff/hussein.png'
+  },
+  {
+    name: 'Vivien Ezeji',
+    position: 'Administrative Officer',
+    image: '/img/stuff/vivien.png'
+  },
+  {
+    name: 'Brezhnev Agbor',
+    position: 'Property Consultant',
+    image: '/img/stuff/brezhnev.png'
+  },
+  {
+    name: 'Ibrahim Ashraf Ibrahim',
+    position: 'Property Consultant',
+    image: '/img/stuff/ibrahim.png'
+  }
+]
 
 onMounted(() => {
   const options = {
@@ -118,6 +190,7 @@ onUnmounted(() => {
     font-size: 1.4rem;
     color: var(--color-black);
     max-width: 88rem;
+    padding-left: 4rem;
 
     @media (max-width: 1356px) and (min-width: 769px) {
       width: 100%;
@@ -135,7 +208,7 @@ onUnmounted(() => {
     display: flex;
     flex-direction: column;
     align-items: center;
-    padding-inline: 2rem;
+    padding-inline: 1rem;
   }
 
   &__section {
@@ -163,6 +236,9 @@ onUnmounted(() => {
         background-color: var(--color-white);
         color: var(--color-black);
         padding-top: 2rem;
+        > p {
+          padding-top: 4rem;
+        }
       }
       .about-us-page__image {
         animation: slideInLeft 1s forwards;
@@ -226,7 +302,7 @@ onUnmounted(() => {
     }
 
     h2 {
-      font-size: 3rem;
+      font-size: 2.6rem;
       margin-bottom: 1rem;
       margin-left: 1rem;
       letter-spacing: 0.02rem;
@@ -241,7 +317,7 @@ onUnmounted(() => {
     }
 
     p {
-      font-size: 1.66rem;
+      font-size: 1.5rem;
       max-width: max-content;
       letter-spacing: .04rem;
 
@@ -261,9 +337,8 @@ onUnmounted(() => {
 
     img {
       width: 100%;
+      max-height: 500px;
       height: auto;
-      max-width: 1400px;
-      max-height: 1400px;
       object-fit: cover;
     }
 
@@ -280,26 +355,53 @@ onUnmounted(() => {
     }
   }
 
-  &__footer {
-    text-align: center;
-    font-size: 1.8rem;
-    color: var(--color-black);
+  &__team {
     margin-top: 4rem;
-    margin-bottom: 4rem;
-    font-weight: 500;
-    letter-spacing: 0.4rem;
+    text-align: center;
 
-    @media (max-width: 1356px) and (min-width: 769px) {
-      font-size: 1.5rem;
-      margin-top: 2rem;
+    h2 {
+      font-size: 2.4rem;
       margin-bottom: 2rem;
+      color: var(--color-black);
+      letter-spacing: 0.2rem;
     }
 
-    @media (max-width: 768px) {
-      font-size: 1.2rem;
-      margin-top: 2rem;
+    .about-us-page__swiper {
       margin-bottom: 2rem;
-      padding: 0 1rem;
+
+      .swiper-slide {
+        display: flex;
+        justify-content: center;
+        width: auto;
+      }
+    }
+
+    .employee-card {
+      display: flex;
+      flex-direction: column;
+      width: 100%;
+      max-width: 280px;
+      align-items: center;
+      padding: 0.5rem;
+      background-color: var(--color-white);
+
+      img {
+        width: 100%; /* Make the image width responsive */
+        height: auto; /* Maintain aspect ratio */
+        object-fit: cover;
+        margin-bottom: 1rem;
+      }
+
+      h3 {
+        font-size: 1.4rem;
+        margin-bottom: 0.5rem;
+        color: var(--color-black);
+      }
+
+      p {
+        font-size: 1.2rem;
+        color: var(--color-grey);
+      }
     }
   }
 }
